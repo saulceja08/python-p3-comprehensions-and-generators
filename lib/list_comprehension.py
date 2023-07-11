7ad84a5b792fef15dd117c0914163561b3fe04ef
Saul Ceja <saulceja08@gmail.com>
2023-07-10 22:54:33 -0700

    Complete Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    list_of_even = [n for n in num_list if n % 2 == 0]
+    return list_of_even
+    
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_sentences = [sentence + "!" for sentence in sentence_list]
+    return exclaimed_sentences
+
